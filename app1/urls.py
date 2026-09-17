e5700901068693fc3e5b31b6c3e832c49a3485d9
JAjajajNNNa <gabriel.rivas08@inacapmail.cl>
2026-09-17 20:54:04 -0300

    subida rivasgabrielrama2

--- app1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'app1'
-
-urlpatterns = [
-    path('vista1/', views.vista1_app1, name='vista1_app1'),
-    path('vista2/', views.vista2_app1, name='vista2_app1'),
-]
